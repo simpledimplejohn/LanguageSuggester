0629ea7e2dd3465744f49a6c370cabeec2af6a84
Simple Dimple John <simpledimplejohn@gmail.com>
2021-06-04 10:59:35 -0700

    add if statment to scripts.js for all languages.

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+  $("form#quiz-form").submit(function(event) {
+    event.preventDefault();
+
+    const time = $("#time").val();
+  
+    if (time === "lots") {
+      $(".previous").hide();
+      $("#fortran").show();
+    } else if (time === "some") {
+      $(".previous").hide();
+      $("#cobol").show();
+    } else if (time === "little") {
+      $(".previous").hide();
+      $("#lisp").show();
+    } else {
+      $(".previous").hide();
+      $("#ada").show();
+    }
+  });
+});
