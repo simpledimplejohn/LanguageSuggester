67ffa5aa0373cb8e523cf89d6069b9a6e0b268c0
Simple Dimple John <simpledimplejohn@gmail.com>
2021-06-04 15:02:19 -0700

    add test page link

--- js/scripts.js
+++ js/scripts.js
@@ -5,17 +5,14 @@ $(document).ready(function() {
     const time = $("#time").val();
   
     if (time === "lots") {
-      $(".previous").hide();
+
       $("#fortran").show();
       
     } else if (time === "some") {
-      $(".previous").hide();
       $("#cobol").show();
     } else if (time === "little") {
-      $(".previous").hide();
       $("#lisp").show();
     } else {
-      $(".previous").hide();
       $("#pascal").show();
     }
   });
