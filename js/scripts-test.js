94c584a78b8725db37a145aeb98d815e39748d44
Simple Dimple John <simpledimplejohn@gmail.com>
2021-06-05 12:43:42 -0700

    add second form to scripts-test branching

--- js/scripts-test.js
+++ js/scripts-test.js
@@ -5,16 +5,16 @@ $(document).ready(function() {
     const time = $("#time").val();
     const decade = ($("#decade").val());
 
-    if (decade === "1960") {
+    if (decade === "1960" && (time === "lots" || time === "some")) {
       $(".previous").hide();
       $("#fortran").show();
-    } else if(decade === "1950") {
+    } else if(decade === "1950" && (time === "lots" || time === "some")) {
       $(".previous").hide();
       $("#cobol").show();
-    } else if(decade === "1970") {
+    } else if(decade === "1970" && (time === "lots" || time === "some")) {
       $(".previous").hide();
       $("#lisp").show();
-    } else if(decade === "1940") {
+    } else if(decade === "1940" && (time === "lots" || time === "some")) {
       $(".previous").hide();
       $("#pascal").show();
     } else if(decade === "1980") {
