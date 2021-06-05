adfe780c2c7538f59561b963592cc36df1b1ab01
Simple Dimple John <simpledimplejohn@gmail.com>
2021-06-05 12:23:38 -0700

    wip second form field testing

--- js/scripts-test.js
+++ js/scripts-test.js
@@ -5,22 +5,22 @@ $(document).ready(function() {
     const time = $("#time").val();
     const decade = $("#decade").val();
 
-    if (decade === "1960") {
+    if ((time === "lots") && (decade === "1960")) {
       $(".previous").hide();
       $("#fortran").show();
-    } else if(decade === "1970") {
+    } else if(time === "lots" && decade === "1970") {
       $(".previous").hide();
       $("#cobol").show();
-    } else if(decade === "1970s") {
+    } else if(time === "some" && decade === "1970s") {
       $(".previous").hide();
       $("#lisp").show();
-    } else if(decade === "1950") {
+    } else if(time === "lots" && decade === "1950") {
       $(".previous").hide();
       $("#pascal").show();
     } else if(decade === "1980s") {
       $(".previous").hide();
       $("#basic").show();
-    }else {
+    } else {
       $(".previous").hide();
       $("#javascript").show();
     }
