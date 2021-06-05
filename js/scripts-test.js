bf8b390dff02ea142e697665ecad10c210489940
Simple Dimple John <simpledimplejohn@gmail.com>
2021-06-05 12:54:24 -0700

    add computers form to test page

--- js/scripts-test.js
+++ js/scripts-test.js
@@ -3,21 +3,22 @@ $(document).ready(function() {
     event.preventDefault();
 
     const time = $("#time").val();
-    const decade = ($("#decade").val());
+    const decade = $("#decade").val();
+    const computer =$("#computer").val();
 
-    if (decade === "1960" && (time === "lots" || time === "some")) {
+    if (decade === "1960" && (time === "lots" || time === "some") && computer === "mainframe") {
       $(".previous").hide();
       $("#fortran").show();
-    } else if(decade === "1950" && (time === "lots" || time === "some")) {
+    } else if(decade === "1950" && (time === "lots" || time === "some") && computer === "mainframe") {
       $(".previous").hide();
       $("#cobol").show();
-    } else if(decade === "1970" && (time === "lots" || time === "some")) {
+    } else if(decade === "1970" && (time === "lots" || time === "some") && computer === "mainframe") {
       $(".previous").hide();
       $("#lisp").show();
-    } else if(decade === "1940" && (time === "lots" || time === "some")) {
+    } else if(decade === "1940" && (time === "lots" || time === "some") && computer === "mainframe") {
       $(".previous").hide();
       $("#pascal").show();
-    } else if(decade === "1980") {
+    } else if(decade === "1980" || computer === "commodore64") {
       $(".previous").hide();
       $("#basic").show();
     } else {
