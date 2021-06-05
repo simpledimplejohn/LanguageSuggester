ba148d1e99f0f619455a47ca268f7ba899d71dbc
Simple Dimple John <simpledimplejohn@gmail.com>
2021-06-05 12:37:22 -0700

    change values in decade quiz form

--- js/scripts-test.js
+++ js/scripts-test.js
@@ -3,21 +3,21 @@ $(document).ready(function() {
     event.preventDefault();
 
     const time = $("#time").val();
-    const decade = $("#decade").val();
+    const decade = ($("#decade").val());
 
-    if ((time === "lots") && (decade === "1960")) {
+    if (decade === "1960") {
       $(".previous").hide();
       $("#fortran").show();
-    } else if(time === "lots" && decade === "1970") {
+    } else if(decade === "1950") {
       $(".previous").hide();
       $("#cobol").show();
-    } else if(time === "some" && decade === "1970s") {
+    } else if(decade === "1970") {
       $(".previous").hide();
       $("#lisp").show();
-    } else if(time === "lots" && decade === "1950") {
+    } else if(decade === "1940") {
       $(".previous").hide();
       $("#pascal").show();
-    } else if(decade === "1980s") {
+    } else if(decade === "1980") {
       $(".previous").hide();
       $("#basic").show();
     } else {
